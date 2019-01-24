d9fe65d1ef21ed11ac84ee18f7103f6466a3fb0b
Denis Sobirov <denvoribos@gmail.com>
2019-01-24 16:44:41 +0200

    init

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
-  def inactive_message
+  def active_for_authentication?
     super && account_active?
   end
 
