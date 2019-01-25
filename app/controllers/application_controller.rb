590f85cce5664b5f4b9fb2e2778910b4a4e0ce11
Denis Sobirov <denvoribos@gmail.com>
2019-01-25 11:23:01 +0200

    add before_filter to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :user_is_block?
+
+  protected
+
+  def :user_is_block?
+    @user = current_user
+    unless @user.account_active?
+      redirect_to redirect_to :action => 'show', :controller => 'users/registrations'
+      flash.alert = ""
+  end
 end
