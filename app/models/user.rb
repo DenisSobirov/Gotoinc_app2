4cf9b47c6c7294a5db16532508ae1d0be8af202e
Denis Sobirov <denvoribos@gmail.com>
2019-02-03 00:40:45 +0200

    prepare app to use habtm

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,10 @@ class User < ApplicationRecord
 
   has_many :links
   has_one :address, :dependent => :destroy
-  has_many :professions, :dependent => :destroy
+  has_and_belongs_to_many :professions
 
 
   accepts_nested_attributes_for :address, :allow_destroy => true
-  accepts_nested_attributes_for :professions, :allow_destroy => true
-
 
   after_initialize do
    self.address ||= self.build_address
